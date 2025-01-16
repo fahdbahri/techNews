d958bc4ca7510cc60c691816545f819c65c44ed0
fahdbahri <fahdkhalid2002@gmail.com>
2025-01-16 23:54:35 +0200

    feat: add scheduling every day at 8:30am

--- src/main.py
+++ src/main.py
@@ -1,14 +1,42 @@
 from controllers.handle_cron import *
 from dotenv import load_dotenv
 import asyncio
-
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime
 
 load_dotenv()
 
 async def main():
-    print("Start process to generate draft...")
-    await handle_cron()
-
+    # Create an async scheduler
+    scheduler = AsyncIOScheduler()
+    
+    # Schedule the job
+    scheduler.add_job(handle_cron, 'cron', hour='8', minute='30')
+    
+    try:
+        scheduler.start()
+        print("Scheduler started. Press Ctrl+C to exit")
+        
+        # Keep the script running
+        while True:
+            await asyncio.sleep(60)
+            
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        print("Scheduler stopped")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(f"Error in scheduler: {e}")
+
+
+# async def main():
+#     await handle_cron()
+
+# if __name__ == "__main__":
+#     try:
+#         asyncio.run(main())
+#     except Exception as e:
+#         print(f"Error in function main: {e}")
